a2a7d3dfb5143406fb7b15a787eecc3f7cd5e125
Shiick <blizz.shiick@hotmail.com>
2022-12-07 09:58:08 +0100

    Update script.py

--- Day7/script.py
+++ Day7/script.py
@@ -1,55 +1,40 @@
 # THIS IS DAY 7!
 
-hierarchy = []
-hierarchy.append("- / (dir)")
-
-def find_all(current_increment):
-    count = 0
-    for item in hierarchy:
-        if len(item) - len(item.lstrip()) == len(current_increment):
-            count += 1
-    return count
-
 def part_one():
-    current_folder = ""
-    current_parents = []
-    folders = {}
-    children = {}
-    with open("inputs.txt") as input_file:
+    parents = {}
+    last_cd = ""
+    sizes = {}
+    with open("test_inputs.txt") as input_file:
         file = input_file.read().split('\n')
         for line in file: # for each line in file
             line = line.split()
             if (line[0] == "$"):
                 if (line[1] == "cd"):
                     if (line[2] != ".."):
-                        if (current_folder != ""):
-                            children[current_folder] = children[current_folder] + [line[2]]
-                            children[line[2]] = []
-                        else:
-                            children[line[2]] = []
-                        current_folder = line[2]
-                        current_parents.append(line[2])
-                        folders[current_folder] = 0
+                        path = last_cd.split("!")
+                        print(path)
+                        path.append(line[2])
+                        last_cd = "!".join(path)
                     else:
-                        current_parents = current_parents[:-1]
-                        current_folder = current_parents[-1]
+                        path = last_cd.split("!")
+                        path.pop()
+                        last_cd = "!".join(path)
             else:
-                if (line[0] != "dir"):
-                    folders[current_folder] += int(line[0])
-    
-    result = 0
-    for child in children:
-        folder_size = get_folder_size(child, children, folders)
-        print(child, folder_size)
-        if (folder_size <= 100000):
-            result += folder_size
-    print(f"Sum of part one {result}")
+                if (line[0] == "dir"):
+                    parents[line[1]] = last_cd
+                else:
+                    if ("!".join(last_cd) in sizes):
+                        sizes["!".join(last_cd)] += int(line[0])
+                    else:
+                        sizes["!".join(last_cd)] = int(line[0])
 
-def get_folder_size(folder, children, folders):
-    size = folders[folder]
-    for child in children[folder]:
-        folder_size = get_folder_size(child, children, folders)
-        size += folder_size
-    return size
+    print(sizes)
+
+    result = 0
+    for size in sizes:
+        if sizes[size] <= 100000:
+            result += sizes[size]
+
+    print(f"Sum of part one {result}")
             
 part_one()
